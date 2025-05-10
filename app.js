af01417d5b5113a80030be48544acff04deef750
Luiz Fernando <luizfranca35mc@gmail.com>
2025-05-09 21:18:25 -0300

    FUNCOES

--- app.js
+++ app.js
@@ -137,10 +137,13 @@ frutas.push("🍊")
 frutas.push("🍓")
 console.log(frutas)
 
-
-
-
 // Funções básicas
+function soma(num1, num2) {
+let total = num1 + num2
+console.log("O resultado é " + total) 
+}
+soma(100 , 14)
+soma(7 , 3)
 // Escopo
 
 
